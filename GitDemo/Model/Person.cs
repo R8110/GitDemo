3158288d8d01b95058899f4d2b62dd1cbf001dbf
BhanuPrakashReddy Yadamuri <azureguruu@gmail.com>
2025-05-15 21:36:55 -0500

    New props occupation, national added to person class

--- GitDemo/Model/Person.cs
+++ GitDemo/Model/Person.cs
@@ -11,5 +11,8 @@ namespace GitDemo.Model
         public string Name {get;set; }  
         public string PhoneNumber {get;set; }
         public string Email {get;set; }
+        public string Occupation {get;set; }
+        public string National { get; set; }
+        
     }
 }
